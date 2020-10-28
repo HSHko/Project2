1b5dc37b327442c439d2420f9f178f6a615f76ea
HSHko <pleasmin@gmail.com>
2020-10-29 03:28:39 +0900

    on settings...

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+interface Props {}
+
+export default function fun() {
+  return <h1>Teste Main</h1>;
+}
---
+++ src/pages/login.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+// import styled from "styled-components";
+// import { useSelector } from "react-redux";
+// import { useDispatch } from "react-redux";
+
+export default function fun() {
+  console.log("init");
+  // const dispatch = useDispatch();
+
+  return (
+    <>
+      <h1>Login</h1>
+    </>
+  );
+}
+
+// const Wrapper = styled.div``;
